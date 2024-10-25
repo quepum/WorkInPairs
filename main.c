3a4791eb36bd83b9a4bf682ca1fc8923259acde9
quepum <118292850+quepum@users.noreply.github.com>
2024-10-25 16:27:25 +0300

    init struct

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include "queue.h"
+#include <stdlib.h>
+
+typedef struct QueueElement {
+    int value;
+    struct QueueElement* next;
+} QueueElement;
+
+struct Queue {
+    QueueElement* head;
+    QueueElement* back;
+};
+
+void createQueue(Queue* queue) {
+    queue->head = (Queue*)calloc(1, sizeof(Queue));
+    queue->back = (Queue*)calloc(1, sizeof(Queue));
+}
+
+
 
