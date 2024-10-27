ef5b1ae271f92e4199e00d274e929ad5d295ef9f
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-10-27 23:30:43 +0300

    New files

--- queue.h
+++ queue.h
@@ -3,6 +3,19 @@
 //
 
 #ifndef WORKINPAIRS_QUEUE_H
-#define WORKINPAIRS_QUEUE_H
+#include <stdbool.h>
+bool testScanf(int result);
+typedef struct QueueElement {
+    int value;
+    struct QueueElement* next;
 
+}QueueElement;
+struct Queue {
+    struct QueueElement* front;
+    struct QueueElement* back;
+};
+void Scanf(int* add);
+void deleteFront(struct Queue* block);
+void addBack(struct Queue* block, int value);
+void queue(struct Queue* block, int value);
 #endif //WORKINPAIRS_QUEUE_H
