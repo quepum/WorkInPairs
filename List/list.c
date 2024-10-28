22cdfb92e15e6e21216e31ff574a3f7d5ac56b45
quepum <118292850+quepum@users.noreply.github.com>
2024-10-28 19:07:23 +0300

    add solution

--- List/list.c
+++ List/list.c
@@ -1,6 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "list.h"
 
-typedef struct Element {
-    int value;
-    struct QueueElement *next;
-};
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+void addEnd(Node** head, int value) {
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        Node* temp = *head;
+        while (temp->next!= NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+Node* removeStart(Node** head) {
+    if (*head == NULL) {
+        return NULL;
+    }
+    Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+    return *head;
+}
+
+void printAddedValues(Node* head) {
+    Node* temp = head;
+    while (temp!= NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
--- List/list.h
+++ List/list.h
@@ -1,3 +1,10 @@
 #pragma once
 
-typedef struct Element;
+typedef struct Node Node;
+// Функция для добавления элемента в конец списка
+void addEnd(Node** head, int value);
+// Функция для удаления элемента из начала списка
+Node* removeStart(Node** head);
+// Функция для вывода значений списка
+void printAddedValues(Node* head);
+
